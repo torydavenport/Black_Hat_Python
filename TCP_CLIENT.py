2298d9b37b8b10cc789bf4e47f6eb6673b682c96
Tory Davenport <contact@torydavenport.com>
2019-07-17 09:44:37 -0400

    Updating

--- TCP_CLIENT.py
+++ TCP_CLIENT.py
@@ -8,8 +8,8 @@ UPDATE 6-28-2019 -- > This script is now update for python3
 
 import socket
 
-target_host = "127.0.0.1"
-target_port = 60555
+target_host = "216.107.213.114"
+target_port = 80
 
 # Create a socket object
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
